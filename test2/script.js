9db69f3cc600590b6fe777853d7617ce4e22e9b8
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2023-08-27 10:59:55 +0800

    fixed backspace function scoping

--- test2/script.js
+++ test2/script.js
@@ -10,8 +10,9 @@
         //if prblems solved is 5
             // ...   
     //if wrong, display wrong
-        //do not proceed to another problem
+        //do not proceed to another problem    
 
+var userAnswer = document.querySelector('.user_answer').innerHTML;
 
 //MAIN GAME FUNCTION
 function startGame() {
@@ -19,20 +20,24 @@ function startGame() {
     var firstOperand;
     var secondOperand;
     let userAnswerStyle = document.querySelector('.user_answer');
+    let allNumKeys = document.querySelectorAll('.keynum');
 
-    //creates a problem
-    function nextProblem() {
-        firstOperand = Math.floor(Math.random() * 8 + 2);
-        secondOperand = Math.floor(Math.random() * 89 + 10);
-        rightAnswer = firstOperand * secondOperand;
+    function clickableKeys() {
+        for (let i = 0; i < allNumKeys.length; i++){
+            allNumKeys[i].addEventListener('click', () => {
+                let num = allNumKeys[i].innerHTML;
+                userAnswer += num;
+                document.querySelector('.user_answer').innerHTML = userAnswer;
+                console.log(typeof(userAnswer), userAnswer)
+            })
+        }
     }
-    
+    clickableKeys();
+        
     //takes input
     function userInput(userAnswer) {
         return userAnswer;
     }
-
-
 
     //checks answer
     function checkAnswer(userAnswer) {
@@ -41,17 +46,13 @@ function startGame() {
         } else {
             userAnswerStyle.style.backgroundColor = 'red'
         }
-    }
-}
-let allNumKeys = document.querySelectorAll('.keynum');
-
-function clickableKeys() {
-    for (let i = 0; i < allNumKeys.length; i++){
-        allNumKeys[i].addEventListener('click', () => {
-            let num = allNumKeys[i].innerHTML;
-            userAnswer += num;
-            console.log(typeof(userAnswer), userAnswer)
-        })
+    }    
+    
+    //creates the next problem
+    function nextProblem() {
+        firstOperand = Math.floor(Math.random() * 8 + 2);
+        secondOperand = Math.floor(Math.random() * 89 + 10);
+        rightAnswer = firstOperand * secondOperand;
     }
 }
 
@@ -59,21 +60,7 @@ function backspace() {
     if (userAnswer.length > 0) {
         let sliced = userAnswer.substring(0, userAnswer.length -1)
         userAnswer = sliced;
-    } else {
-        console.log('there is nothing to delete')
+        document.querySelector('.user_answer').innerHTML = userAnswer;
+        console.log(userAnswer)
     }
 }
-
-var userAnswer = document.querySelector('.user_answer').innerHTML;
-
-
-var a = 10;
-function myfunc() {
-    a = 15
-    function secfunc() {
-        a =20
-    }
-    secfunc();
-}
-myfunc();
-console.log(a)
